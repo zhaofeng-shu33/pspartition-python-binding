e9c0b3a7cdc0770233488b31948c393d4f89f201
zhaofeng-shu33 <616545598@qq.com>
2019-06-08 00:24:55 +0800

    remove "

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class build_ext(build_ext_orig):
                     raise NameError("VCPKG_ROOT environment variable not set")
                 VCPKG_DEFAULT_TRIPLET = os.environ.get('VCPKG_DEFAULT_TRIPLET', 'x64-windows')
                 if(os.environ.get('APPVEYOR')):
-                    cmake_args += ['-G', '"Visual Studio 15 2017 Win64"']
+                    cmake_args += ['-G', 'Visual Studio 15 2017 Win64']
                 cmake_args += [
                     '-DCMAKE_TOOLCHAIN_FILE=' + os.path.join(VCPKG_ROOT, 'scripts', 'buildsystems', 'vcpkg.cmake'),
                     '-DVCPKG_TARGET_TRIPLET=' + VCPKG_DEFAULT_TRIPLET
