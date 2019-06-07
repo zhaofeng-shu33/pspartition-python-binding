c166bbb83c7102ab8b4b2b676417f78fbc02bab3
zhaofeng-shu33 <616545598@qq.com>
2019-06-07 23:27:24 +0800

    add passed setting

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ class build_ext(build_ext_orig):
                 if(VCPKG_ROOT == None):
                     raise NameError("VCPKG_ROOT environment variable not set")
                 VCPKG_DEFAULT_TRIPLET = os.environ.get('VCPKG_DEFAULT_TRIPLET', 'x64-windows')
+                if(os.environ.get('Python3_ROOT_DIR')):
+                    cmake_args += ['-DPython3_ROOT_DIR=' + os.environ['Python3_ROOT_DIR']]
                 cmake_args += [
                     '-DCMAKE_TOOLCHAIN_FILE=' + os.path.join(VCPKG_ROOT, 'scripts', 'buildsystems', 'vcpkg.cmake'),
                     '-DVCPKG_TARGET_TRIPLET=' + VCPKG_DEFAULT_TRIPLET
