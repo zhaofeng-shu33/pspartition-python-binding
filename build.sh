3874734ed26694eff6739a7e8df965f3827f3077
zhaofeng-shu33 <616545598@qq.com>
2019-06-10 22:28:33 +0800

    add build script, used in docker

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e -x
+PLAT=manylinux2010_x86_64
+# this script should be executed within lemon:v2 docker
+# Compile wheels
+PY36=/opt/python/cp36-cp36m/bin
+PY37=/opt/python/cp37-cp37m/bin
+$PY36/pip wheel . -w dist/
+$PY37/pip wheel . -w dist/
+# Bundle external shared libraries into the wheels
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT
+done
+
