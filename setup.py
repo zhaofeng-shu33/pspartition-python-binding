3be6e740e93a1b5c950cc9f8bc74184f1981a05b
zhaofeng-shu33 <616545598@qq.com>
2019-06-14 23:32:51 +0800

    restricted to macos

--- setup.py
+++ setup.py
@@ -48,12 +48,14 @@ def set_up_cython_extension():
         sourcefiles.append(set_file)
     else:
         raise FileNotFoundError(set_file)
-        
+    extra_compile_flags_list = []
+    if(sys.platform == 'darwin'):
+        extra_compile_flags_list.append('std=c++11')
     extensions = [
         Extension('info_cluster.psp', sourcefiles, 
             include_dirs=extra_include_path,
             library_dirs=extra_lib_dir,
-            extra_compile_args=["-std=c++11"],
+            extra_compile_args=extra_comple_flags_list,
             libraries = [lemon_lib_name]
         )
     ]
